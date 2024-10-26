2aa51b3001a03565a0ace3d54c5990bc26e0be35
Sreeram Venkitesh <sreeram.venkitesh@bigbinary.com>
2024-10-26 20:01:42 +0530

    Setup a basic HTTP server

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Reverse Proxy
 
-This is a simple HTTP reverse proxy written in Go as part of my technical challenge for Traefik.g
+This is a simple HTTP reverse proxy written in Go as part of my technical challenge for Traefik
--- main.go
+++ main.go
@@ -2,8 +2,29 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+	"time"
 )
 
+func handleRequest(w http.ResponseWriter, r *http.Request) {
+	log.Printf("%s %s %s", r.RemoteAddr, r.Method, r.URL)
+	fmt.Fprintf(w, "Hello world!")
+}
+
 func main() {
-	fmt.Println("Reverse proxy is starting...")
+	log.Printf("Reverse proxy is starting...")
+
+	port := 8080
+
+	s := &http.Server{
+		Addr:           fmt.Sprintf(":%d", port),
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+		Handler:        http.HandlerFunc(handleRequest),
+	}
+	
+	log.Printf("Started server on port: %d\n", port)
+	log.Fatal(s.ListenAndServe())
 }
